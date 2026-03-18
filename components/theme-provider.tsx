d6f52e17460c739c06b577805fe5baa8b3dd750b
Rahul Kumar Behera <42063508+cruelyf@users.noreply.github.com>
2026-03-18 10:50:31 +0530

    Remove outdated privacy and terms pages; add new about, blog, careers, contact, and updated privacy and terms pages with improved content and structure.

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,21 +1,8 @@
-'use client'
-
-import * as React from 'react'
 import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
-} from 'next-themes'
+} from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  const [mounted, setMounted] = React.useState(false)
-
-  React.useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (!mounted) {
-    return children
-  }
-
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
